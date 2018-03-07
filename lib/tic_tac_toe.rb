cc84faaf1af6d382bc014e96b2c685bdc24d7502
Trevor Seitz <trevinla@gmail.com>
2018-03-07 16:22:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,4 +35,17 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, current_player)
+    board[index] = current_player
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+
 end
