d2b2e2268fbfd191fdc06b6b1fa0ad7443ee1eae
Trevor Seitz <trevinla@gmail.com>
2018-03-07 17:14:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ def draw?
 end
 
 def won?
-binding.pry
+# binding.pry
   WIN_COMBINATIONS.detect do |win_combo|
     if @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(@board, win_combo[1])
       return win_combo
