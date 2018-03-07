20096923b8bc0d70dc06424873277daefda68a1e
Trevor Seitz <trevinla@gmail.com>
2018-03-07 16:16:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  def WIN_COMBINATIONS
+  WIN_COMBINATIONS =
   [[0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -29,6 +29,10 @@ end
 
 def turn_count
   @board.count{|token| token == "X" || token == "O"}
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 end
